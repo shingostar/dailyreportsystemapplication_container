1349a55413f2a6c63236cc05e2c6c64c52dc4e00
shingostar <shingostar@shore.ocn.ne.jp>
2024-03-25 22:46:37 +0900

    【最終課題】Dockerfileとdocker-compose.ymlをメンターにチェックしてもらいましょう

---
+++ db/mysql_setup.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE IF NOT EXISTS tutorial_develop CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+CREATE USER IF NOT EXISTS 'devuser'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON *.* TO 'devuser'@'%';
