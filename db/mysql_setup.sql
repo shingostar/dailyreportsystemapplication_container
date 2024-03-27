106288f0a0ffb6db5c3ea8cd2e70fdf0a3bfce75
shingostar <shingostar@shore.ocn.ne.jp>
2024-03-27 12:45:29 +0900

    db1_containerに変更後、デプロイ。

--- db/mysql_setup.sql
+++ db/mysql_setup.sql
@@ -1,3 +1,3 @@
-CREATE DATABASE IF NOT EXISTS tutorial_develop CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
-CREATE USER IF NOT EXISTS 'devuser'@'%' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON *.* TO 'devuser'@'%';
+CREATE DATABASE IF NOT EXISTS dailyreportsystemapplication_container CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+CREATE USER IF NOT EXISTS 'repuser'@'%' IDENTIFIED BY 'reppass';
+GRANT ALL PRIVILEGES ON *.* TO 'repuser'@'%';
