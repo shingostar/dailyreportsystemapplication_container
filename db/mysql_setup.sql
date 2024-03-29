f136893e8912b3d462228e42984310afa0d304c9
shingostar <shingostar@shore.ocn.ne.jp>
2024-03-29 10:58:31 +0900

    更新します。

--- db/mysql_setup.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE IF NOT EXISTS dailyreportsystemapplication_container CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
-CREATE USER IF NOT EXISTS 'repuser'@'%' IDENTIFIED BY 'reppass';
-GRANT ALL PRIVILEGES ON *.* TO 'repuser'@'%';
